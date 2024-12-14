5a95f02f5155d279bdb9f3f12ab05aad9fe70486
ehabmohamed6699 <ihabmohamed6699@gmail.com>
2024-12-14 14:06:37 +0200

    finished initial files and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { StatsComponent } from './components/stats/stats.component';
+import { WalletComponent } from './components/wallet/wallet.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'stats', pathMatch: 'full'},
+    {path: 'stats', component: StatsComponent},
+    {path: 'wallet', component: WalletComponent},
+    {path: '**', component: PageNotFoundComponent}
+];
